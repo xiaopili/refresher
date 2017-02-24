56d68e58047ec96c5e0c12f8c7977bd1cf5f9479
liangxiao <liangxiao@gomeplus.com>
2017-02-24 15:43:20 +0800

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.use('/static', express.static('html'));
+app.use('/static', express.static('src'));
 app.listen(3000);
 console.log('listening on 3000');
